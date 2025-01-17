970cae2f27a54eb2f4ec9899db50ec6994089ca6
DoNam <abcnam26@gmail.com>
2025-01-17 09:51:53 +0700

    123

--- database/migrations/2025_01_16_093742_add_code_to_products_table_new.php
+++ database/migrations/2025_01_16_093742_add_code_to_products_table_new.php
@@ -14,7 +14,7 @@ class AddCodeToProductsTableNew extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('code')->unique()->change();
+            $table->string('code');
         });
     }
 
