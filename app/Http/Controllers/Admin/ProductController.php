c1a3e2118420d9d68f07cf92f7307c6ab0156f68
DoNam <abcnam26@gmail.com>
2025-01-17 10:48:02 +0700

    123

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -249,14 +249,10 @@ class ProductController extends Controller
 
     public function update(Request $request, $code)
     {
-        Log::info('Update method called with code: ' . $code);
-        Log::info('Request data: ', $request->all());
-
         $product = Product::where('code', $code)->firstOrFail();
         $validator = Validator::make($request->all(), $this->getValidationRules());
 
         if ($validator->fails()) {
-            Log::error('Validation failed: ', $validator->errors()->toArray());
             return redirect()->back()
                 ->withErrors($validator)
                 ->withInput();
@@ -272,7 +268,6 @@ class ProductController extends Controller
                 'show_in_web' => $request->has('show_in_web'),
             ]);
             
-            Log::info('Data to update: ', $data);
             $product->update($data);
 
             // Xử lý upload ảnh mới nếu có
@@ -304,12 +299,11 @@ class ProductController extends Controller
             }
 
             DB::commit();
-            Log::info('Product updated successfully');
             return redirect()->route('admin.products.index')
                 ->with('success', 'Cập nhật thông tin bất động sản thành công!');
         } catch (\Exception $e) {
             DB::rollBack();
-            Log::error('Error updating product: ' . $e->getMessage());
+            Log::error('Lỗi khi cập nhật sản phẩm: ' . $e->getMessage());
             return redirect()->back()
                 ->with('error', 'Có lỗi xảy ra: ' . $e->getMessage())
                 ->withInput();
