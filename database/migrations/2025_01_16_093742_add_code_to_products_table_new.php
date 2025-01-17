8befac8d7d669e0edc361646c05756d90847cc48
DoNam <abcnam26@gmail.com>
2025-01-17 09:47:51 +0700

    u

--- database/migrations/2025_01_16_093742_add_code_to_products_table_new.php
+++ database/migrations/2025_01_16_093742_add_code_to_products_table_new.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddCodeToProductsTable extends Migration
+class AddCodeToProductsTableNew extends Migration
 {
     /**
      * Run the migrations.
