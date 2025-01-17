02d9ca0a3b4b24dfa5ffa4f40b8725e820efa967
DoNam <abcnam26@gmail.com>
2025-01-17 09:45:11 +0700

    123

---
+++ database/migrations/2025_01_17_094438_add_code_to_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCodeToProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('code')->unique(); // Mã sản phẩm unique
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            // Xóa cột 'code' nếu cần quay lại
+            $table->dropColumn('code');
+        });
+    }
+}
